4c731b0ae8734df0d78da7b1d2d37e3378adcaf4
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-14 12:09:28 +0300

    + HttpRequestInterface2

--- src/Http/HttpRequestInterface.php
+++ src/Http/HttpRequestInterface.php
@@ -4,23 +4,16 @@ declare(strict_types=1);
 namespace IfCastle\Protocol\Http;
 
 use IfCastle\Async\ReadableStreamInterface;
+use IfCastle\Protocol\HeadersInterface;
+use IfCastle\Protocol\RequestParametersInterface;
 
-interface HttpRequestInterface
+interface HttpRequestInterface extends HeadersInterface, RequestParametersInterface
 {
     public function getMethod(): string;
-    public function getParameter(string $name): ?string;
-    public function getParameters(): array;
+    public function getCookies(): array;
     
     public function getBodySize(): int;
     public function getBody(): string;
-    
-    public function getCookies(): array;
-    
-    public function getHeaders(): array;
-    
-    public function getHeader(string $name): ?array;
-    
-    public function getHeaderLine(string $name): ?string;
     
     public function getBodyStream(): ?ReadableStreamInterface;
 }
