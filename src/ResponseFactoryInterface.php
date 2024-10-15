18c662d04ee7043526f4cecb3c923e7106bfc156
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-15 19:52:02 +0300

    + ResponseFactoryInterface

--- src/ResponseFactoryInterface.php
+++ src/ResponseFactoryInterface.php
@@ -3,7 +3,15 @@ declare(strict_types=1);
 
 namespace IfCastle\Protocol;
 
+use IfCastle\Async\ReadableStreamInterface;
+
 interface ResponseFactoryInterface
 {
+    public function createSuccessResponse(
+        string|bool|int|float|array|null|ReadableStreamInterface $response,
+        ?string                                                  $contentType = null,
+        ?int                                                     $responseCode = null
+    ): ResponseInterface;
     
+    public function createFailedResponse(\Throwable $throwable, string $contentType = null): ResponseInterface;
 }
