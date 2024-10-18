f9fc3e30ebcb90d0671f85a0bdf82aa8346d6810
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-18 17:55:28 +0300

    + HttpErrorInterface.php

---
+++ src/Http/Exceptions/HttpErrorInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\Protocol\Http\Exceptions;
+
+interface HttpErrorInterface
+{
+    public function getStatusCode(): int;
+    
+    public function getReasonPhrase(): string|null;
+}
